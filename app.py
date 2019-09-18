3ed32cb2e5ba74f769134af1cba3fc0134eaadfa
Julio Alcibar <jalcibar@VILLA.local>
2019-09-18 11:18:36 -0500

    postgress

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from datetime import datetime
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rtbparnxbzpkfj:7af491d615adeaf621cfcbe7f47c79348ffaa7f9c0da4bf438612c77c43762c4@ec2-54-235-104-136.compute-1.amazonaws.com:5432/daovsp0ht67uql'
+app.config['SECRET_KEY'] = '7af491d615adeaf621cfcbe7f47c79348ffaa7f9c0da4bf438612c77c43762c4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
@@ -32,50 +33,12 @@ class sigFoxGet(Resource):
         
         return item.json()
 
-class Movies_List(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('deviceId', type=str, required=False, help='identificador del dispositivo')
-    parser.add_argument('data', type=str, required=False, help='datos en tiempo real')
-    parser.add_argument('Time', type=int, required=True, help='Fecha')
-    
-    def get(self, movie):
-        item = Movies.find_by_title(movie)
-        if item:
-            return item.json()
-        return {'Message': 'Movie is not found'}
-    
-    def post(self, movie):
-        if Movies.find_by_title(movie):
-            return {' Message': 'Movie with the  title {} already exists'.format(movie)}
-        args = Movies_List.parser.parse_args()
-        item = Movies(movie, args['director'], args['genre'], args['collection'])
-        item.save_to()
-        return item.json()
-        
-    def put(self, movie):
-        args = Movies_List.parser.parse_args()
-        item = Movies.find_by_title(movie)
-        if item:
-            item.collection = args['collection']
-            item.save_to()
-            return {'Movie': item.json()}
-        item = Movies(movie, args['director'], args['genre'], args['collection'])
-        item.save_to()
-        return item.json()
-            
-    def delete(self, movie):
-        item  = Movies.find_by_title(movie)
-        if item:
-            item.delete_()
-            return {'Message': '{} has been deleted from records'.format(movie)}
-        return {'Message': '{} is already not on the list'.format()}
     
 class All_Movies(Resource):
     def get(self):
         return list(map(lambda x: x.json(), Access.query.all()))
     
 api.add_resource(All_Movies, '/values')
-api.add_resource(Movies_List, '/<string:movie>')
 api.add_resource(sigFoxGet,'/sigFoxGet')
 if __name__ == '__main__':
     app.run(debug=True,host='localhost',port=5000)
