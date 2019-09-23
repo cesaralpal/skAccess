85311f147aac95e1e3ad20058faa43d7bd9e8da2
Julio Alcibar <jalcibar@VILLA.local>
2019-09-23 08:43:49 -0500

    se agrega update de valores/ya no apila el historial

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Resource, reqparse, Api
 import time
 from datetime import datetime
-from sqlalchemy import create_engine
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,7 +10,7 @@ app.config['SECRET_KEY'] = '7af491d615adeaf621cfcbe7f47c79348ffaa7f9c0da4bf43861
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-from base import Access, db
+from base import Access,AccessHistory,db
 db.init_app(app)
 app.app_context().push()
 db.create_all()
@@ -24,23 +23,55 @@ class sigFoxGet(Resource):
         parser.add_argument('time', type=int)
         parser.add_argument('data', type=str)
 
-
         args = parser.parse_args()
         fecha = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(args['time']))
         temperatura = bytes.fromhex(args['data']).decode('utf-8')
-        item = Access(args['deviceId'], temperatura, fecha)
         print("La temperatura es"+ temperatura)
         print("La fecha es" + fecha)
-        item.save_to()
+        itemb2 = AccessHistory(args['deviceId'], temperatura, fecha)
+        itemb2.save_to()
+        item = Access.find_by_title(args['deviceId'])        
+        if item:            
+            item.data = temperatura
+            item.time = fecha            
+            item.save_to()        
+            return item.json()
+        else:
+            item = Access(args['deviceId'], temperatura, fecha)
+            item.save_to()        
+            return item.json()
         
-        return item.json()
-
     
+  #def get(self):
+        #parser.add_argument('deviceId', type=str)
+        #parser.add_argument('time', type=int)
+        #parser.add_argument('data', type=str)
+
+
+        #args = parser.parse_args()
+        #fecha = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(args['time']))
+        #temperatura = bytes.fromhex(args['data']).decode('utf-8')
+        #item = Access(args['deviceId'], temperatura, fecha)
+        #print("La temperatura es"+ temperatura)
+        #print("La fecha es" + fecha)
+        #item.save_to()
+        #return item.json()
+
+
+        
 class All_Movies(Resource):
     def get(self):
         return list(map(lambda x: x.json(), Access.query.all()))
+class DeviceHistory(Resource):
+    def get(self):
+        return list(map(lambda x: x.json(), Access.find_by_title(deviceId).all()))
+class AllRegister(Resource):
+     def get(self):
+        return list(map(lambda x: x.json(), AccessHistory.query.all()))
     
 api.add_resource(All_Movies, '/values')
 api.add_resource(sigFoxGet,'/sigFoxGet')
+api.add_resource(AllRegister, '/history')
+
 if __name__ == '__main__':
     app.run(debug=True,host='localhost',port=5000)
--- base.py
+++ base.py
@@ -1,9 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
-
 from flask.ext.sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Access(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    deviceId = db.Column(db.String(30), unique=False, nullable=False)
+    data = db.Column(db.String(30), unique=False, nullable=False)
+    time = db.Column(db.String(30), unique=False, nullable=False)
+    
+    def __init__(self, deviceId, data, time):
+        self.deviceId = deviceId
+        self.data = data
+        self.time = time
+        
+    def json(self):
+        return {'DeviceID': self.deviceId, 'Data': self.data, 'Time': self.time}
+    
+    @classmethod
+    def find_by_title(cls, deviceId):
+        return cls.query.filter_by(deviceId=deviceId).first()
+    
+    def save_to(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete_(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class AccessHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     deviceId = db.Column(db.String(30), unique=False, nullable=False)
     data = db.Column(db.String(30), unique=False, nullable=False)
